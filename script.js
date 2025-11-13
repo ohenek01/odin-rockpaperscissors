b2604278d2a5b739ed53f100cce7c1e3f7b916b0
ohenek01 <okosafo-affum@st.ug.edu.gh>
2025-11-13 15:36:04 +0000

    Add human choice function

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ function getComputerChoice(){
     return options[Math.floor(Math.random() * options.length)];
 }
 console.log(getComputerChoice());
+
+function getHumanChoice(){
+    let choice = prompt("Rock, Paper, Scissors");
+    return "You chose " + choice;
+}
+console.log(getHumanChoice());
